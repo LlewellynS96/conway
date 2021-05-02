0532ed44e9530111b9da6136b66de5f3e338a86f
LlewellynS96 <llewellyn121@gmail.com>
2021-05-02 11:04:49 +0200

    improved README.md

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Conway's Game of Life
 
 A PEP-8 compliant implementation of Conway's Game of Life on a toroidal surface with support for argument parsing, in 37 lines of code.
+
+## Usage
+Run
+```python conway.py --size 9 --frames 50 --wait 100``` to generate 50 frames on a board of size 9×9 with a 100 ms pause between frames.
+
+## Examples
+### Random:
+![](images/random.gif)
+### Glider:
+![](images/glider.gif)
+### Blinker:
+![](images/blinker.gif)
+### Out of Bounds:
+![](images/oob.gif)
--- conway.py
+++ conway.py
@@ -21,8 +21,8 @@ def perturb_board(board):
 def update(n):
     if n == 0:
         return
-    perturb_board(BOARD)
-    im.set_data(BOARD)
+    perturb_board(board)
+    im.set_data(board)
 
 
 parser = argparse.ArgumentParser()
@@ -30,27 +30,8 @@ parser.add_argument('-s', '--size', type=int, default=100)
 parser.add_argument('-f', '--frames', type=int, default=1000000)
 parser.add_argument('-w', '--wait', type=int, default=1)
 args = parser.parse_args()
-BOARD = create_board(args.size)
+board = create_board(args.size)
 fig, ax = plt.subplots(figsize=(8, 8), subplot_kw={'xticks': [], 'yticks': []})
-im = ax.imshow(BOARD)
+im = ax.imshow(board)
 animation = FuncAnimation(fig, func=update, frames=args.frames, interval=args.wait, repeat=False)
 plt.show()
-
-
-# BOARD = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 0, 1, 1, 1, 1, 1, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 0, 0, 0]]
-# BOARD = np.array(BOARD)
-
-# BOARD = np.zeros((args.size, args.size))
-# BOARD[8:13, 5:10] = np.array([[0, 0, 1, 0, 0],
-#                   [1, 0, 1, 0, 0],
-#                   [0, 1, 1, 0, 0],
-#                   [0, 0, 0, 0, 0],
-#                   [0, 0, 0, 0, 0]])
